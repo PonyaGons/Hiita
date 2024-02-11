28aceff99b2f108e0c2ede6051e8005a71eda132
aonori <aonori@aonr.jp>
2024-02-11 13:22:20 +0900

    Update for production deploy

--- front/hiita/components/footer.vue
+++ front/hiita/components/footer.vue
@@ -3,12 +3,19 @@
         <v-container>
             <v-row align="end">
                 <v-col> <v-btn block @click="navigateTo('/history')">くじログ</v-btn> </v-col>
-                <v-col> <v-btn block @click="navigateTo('/')">神々の意見</v-btn> </v-col>
+                <v-col> <v-btn block @click="navigateTo('/')" class="main-tab">神々の意見</v-btn> </v-col>
                 <v-col> <v-btn block @click="navigateTo('/add')">追加</v-btn> </v-col>
             </v-row>
         </v-container>
     </v-footer>
 </template>
+
+<style>
+.main-tab {
+    color: #fff;
+    background-color: #B02C23;
+}
+</style>
 
 <script>
 export default {
